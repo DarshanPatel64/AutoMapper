34411d6d807a782904f95a857be4d5a7f56d70c2
darshan patel <pateld1612@gmail.com>
2022-05-19 16:10:53 +0530

    mapped student class to user class

---
+++ ConsoleApp1/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    internal class User
+    {
+        public int UserId { get; set; } 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ConsoleApp1.Data;
+
+namespace ConsoleApp1
+{
+    public class ConsoleApp1
+    {
+        public static void Main()
+        {
+            
+            User user = new User();
+            user.Name = "Darshan";
+            user.Address = "Gandhi Gate";
+            user.Age = 21;
+            user.CreatedDate = DateTime.Now;
+            user.UserId = 11;
+            var config = new MapperConfiguration(
+             cfg =>
+            {
+                cfg.CreateMap<User,Student>()
+                .ForMember(s => s.StudentId, u => u.MapFrom(u => u.UserId));
+            });
+            var mapper = new Mapper(config);
+            Student std = mapper.Map<Student>(user);
+
+            Console.WriteLine(std.StudentId);
+            
+           
+        }
+
+    }
+}
